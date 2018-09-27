62466976f0e0fc203774c013ad6d535a6e847ab7
arkhamPepe <alexander.s.b@hotmail.com>
2018-09-27 20:35:22 +0200

    Comments
    +comments

--- potato1/app/src/main/java/com/example/alexander/learningandroid/SectionsPageAdapter.java
+++ potato1/app/src/main/java/com/example/alexander/learningandroid/SectionsPageAdapter.java
@@ -8,11 +8,19 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
+/** SectionsPageAdapter:
+ * Keeps track of Fragments and their respective titles.
+ */
 public class SectionsPageAdapter extends FragmentPagerAdapter {
 
     private final List<Fragment> mFragmentList = new ArrayList<>(); // Keep track of fragments
     private final List<String> mFragmentTitleList = new ArrayList<>(); // Keep track of the name of the fragments
 
+    /** addFragment:
+     * Stores fragment and title for that Fragment.
+     * @param fragment
+     * @param title
+     */
     public void addFragment(Fragment fragment, String title){
         mFragmentList.add(fragment);
         mFragmentTitleList.add(title);
@@ -23,16 +31,31 @@ public class SectionsPageAdapter extends FragmentPagerAdapter {
         super(fm);
     }
 
+
+    /** getPageTitle:
+     * A simple getter.
+     * @param position
+     * @return title of Fragment
+     */
     @Override
     public CharSequence getPageTitle(int position) {
         return mFragmentTitleList.get(position);
     }
 
+    /** getItem:
+     * A simple getter.
+     * @param i
+     * @return Fragment at i
+     */
     @Override
     public Fragment getItem(int i) {
         return mFragmentList.get(i);
     }
 
+    /** getCount:
+     * A simple getter.
+     * @return amount of Fragments
+     */
     @Override
     public int getCount() {
         return mFragmentList.size();
