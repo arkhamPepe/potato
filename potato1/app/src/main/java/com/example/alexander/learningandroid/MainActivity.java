1e31f57b8a16a75947236bd3cb698267045af605
arkhamPepe <alexander.s.b@hotmail.com>
2018-09-27 20:35:08 +0200

    MainActivity functionality
    +comments
    +setupViewPager() in MainActivity
    +modified onCreate()
    +ViewPager mViewPager
    +String TAG

--- potato1/app/src/main/java/com/example/alexander/learningandroid/MainActivity.java
+++ potato1/app/src/main/java/com/example/alexander/learningandroid/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,13 +22,41 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private SectionsPageAdapter mSectionsPageAdapter;
 
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // No idea what this does
+        Log.d(TAG, "onCreate: .");
+
+        // Instantiate the SectionsPageAdapter field
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter. What is a ViewPager? I don't know...
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+
+        // Get TabLayout from xml and connect it to ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Add Fragments to the ViewPager which is tied to a SectionsPageAdapter
+        adapter.addFragment(new Tab1Fragment(), "TAB1");
+        adapter.addFragment(new Tab2Fragment(), "TAB2");
+        adapter.addFragment(new Tab3Fragment(), "TAB3");
+
+        viewPager.setAdapter(adapter);
     }
 }
