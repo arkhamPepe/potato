c9a24e4b8354e1dbb00fce3f9b0df6c590d849ae
arkhamPepe <alexander.s.b@hotmail.com>
2018-09-27 19:27:53 +0200

    Fragments base
    +tab1_fragment
    +tab2_fragment
    +tab3_fragment
    +Tab1Fragment
    +Tab2Fragment
    +Tab3Fragment

---
+++ potato1/app/src/main/java/com/example/alexander/learningandroid/Tab2Fragment.java
@@ -0,0 +1,33 @@
+package com.example.alexander.learningandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Tab2Fragment extends Fragment {
+    private static final String TAG = "Tab2Fragment";
+
+    private Button btnTEST2;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab2_fragment, container,false);
+
+        btnTEST2 = (Button) view.findViewById(R.id.btnTEST2);
+
+        btnTEST2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
---
+++ potato1/app/src/main/java/com/example/alexander/learningandroid/Tab3Fragment.java
@@ -0,0 +1,33 @@
+package com.example.alexander.learningandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Tab3Fragment extends Fragment {
+    private static final String TAG = "Tab3Fragment";
+
+    private Button btnTEST3;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab3_fragment, container,false);
+
+        btnTEST3 = (Button) view.findViewById(R.id.btnTEST3);
+
+        btnTEST3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
