8920bcebe919f179f6bf381c11c5ec9dd4f59c2d
arkhamPepe <alexander.s.b@hotmail.com>
2018-09-27 21:47:16 +0200

    Tutorial stuff

--- potato1/app/src/main/java/com/example/alexander/learningandroid/Views/SecondActivity.java
+++ potato1/app/src/main/java/com/example/alexander/learningandroid/Views/SecondActivity.java
@@ -3,12 +3,17 @@ package com.example.alexander.learningandroid.Views;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import com.example.alexander.learningandroid.R;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Started.");
     }
 }
