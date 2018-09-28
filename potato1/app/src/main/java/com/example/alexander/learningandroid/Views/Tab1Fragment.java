0f1a4974f8cd51267c5007189c08304d90c622f0
arkhamPepe <alexander.s.b@hotmail.com>
2018-09-28 09:35:39 +0200

    Added buttons and button strings

--- potato1/app/src/main/java/com/example/alexander/learningandroid/Views/Tab1Fragment.java
+++ potato1/app/src/main/java/com/example/alexander/learningandroid/Views/Tab1Fragment.java
@@ -1,5 +1,6 @@
 package com.example.alexander.learningandroid.Views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,6 +16,7 @@ public class Tab1Fragment extends Fragment {
     private static final String TAG = "Tab1Fragment";
 
     private Button btnTEST;
+    private Button btnAct2;
 
     @Nullable
     @Override
@@ -22,13 +24,25 @@ public class Tab1Fragment extends Fragment {
         View view = inflater.inflate(R.layout.tab1_fragment, container,false);
 
         btnTEST = (Button) view.findViewById(R.id.btnTEST);
+        btnAct2 = view.findViewById(R.id.btnAct2);
+
 
         btnTEST.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1", Toast.LENGTH_SHORT).show();
             }
         });
+        btnAct2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
+    }
+
+    public void showActivity2(){
+
     }
 }
